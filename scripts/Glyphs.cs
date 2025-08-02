13d874ba75344163e5067dcaa0766f747596b997
HalaszMateSandor <maseny0@gmail.com>
2025-08-02 20:45:15 +0200

    Started on refactoring the ui, so the glyph management will be easier

--- scripts/Glyphs.cs
+++ scripts/Glyphs.cs
@@ -1,18 +1,10 @@
 using Godot;
 using System;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
 
 public partial class Glyphs : Node3D
 {
 	private List<Node3D> _glyphs = [];
-	private Node3D _igniteGlyph;
-	private Node3D _teleportGlyph;
-	private Node3D _windGlyph;
-	private Node3D _freezeGlyph;
-	private Node3D _earthGlyph;
-	private bool _isGlyphShown = false;
-
 	private Node3D _igniteGlyph;
 	private Node3D _teleportGlyph;
 	private Node3D _windGlyph;
@@ -62,49 +54,6 @@ public partial class Glyphs : Node3D
 			}
 		}
 	}
-<<<<<<< Updated upstream
-		_glyphs.Add(_igniteGlyph = GetNode<Node3D>("I"));
-		_glyphs.Add(_teleportGlyph = GetNode<Node3D>("T"));
-		_glyphs.Add(_windGlyph = GetNode<Node3D>("P"));
-		_glyphs.Add(_freezeGlyph);
-		_glyphs.Add(_earthGlyph);
-		
-		GlyphSetup();
-	}
-
-	private void OnGlyphWakeUpRequest(int spellLetter)
-	{
-		if (_isGlyphShown == false)
-		{
-			switch (spellLetter)
-			{
-				case (int)SpellList.Spells.Ignite:
-					_igniteGlyph.Visible = true;
-					_isGlyphShown = true;
-					break;
-				case (int)SpellList.Spells.Teleport:
-					_teleportGlyph.Visible = true;
-					_isGlyphShown = true;
-					break;
-				case (int)SpellList.Spells.Wind:
-					_windGlyph.Visible = true;
-					_isGlyphShown = true;
-					break;
-				case (int)SpellList.Spells.Freeze:
-					_freezeGlyph.Visible = true;
-					_isGlyphShown = true;
-					break;
-				case (int)SpellList.Spells.Earth:
-					_earthGlyph.Visible = true;
-					_isGlyphShown = true;
-					break;
-				default:
-					break;
-			}
-		}
-	}
-=======
->>>>>>> Stashed changes
 
 	private void OnGlyphSleepRequest(int spellLetter)
 	{
