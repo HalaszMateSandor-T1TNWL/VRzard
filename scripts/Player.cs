f6bffc9866ee0b435a47626dd7809e09bb68702c
Kriston-Széles Benedek <benmano03@gmail.com>
2025-08-28 16:08:20 +0200

    Added interactable object sync and disabled tracking on mp agents

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -4,38 +4,77 @@ using System.Collections.Generic;
 
 public partial class Player : Node3D
 {
-	[Signal]
-	public delegate void GlyphWakeUpEventHandler(int spellLetter);
-	[Signal]
-	public delegate void SpellSelectedEventHandler(int spell);
-	[Signal]
-	public delegate void SpellDeSelectedEventHandler(int spell);
+    [Signal]
+    public delegate void GlyphWakeUpEventHandler(int spellLetter);
+    [Signal]
+    public delegate void SpellSelectedEventHandler(int spell);
+    [Signal]
+    public delegate void SpellDeSelectedEventHandler(int spell);
 
-	private Node3D _canvas;
-	private Error _error;
+    private Node3D _canvas;
+    private Error _error;
+
     public override void _Ready()
-	{
-
-        _canvas = GetNode<Node3D>($"XROrigin3D/CastingCanvas");
-		Node currentScene = GetTree().CurrentScene;
+    {
+        _canvas = GetNode<Node3D>("XROrigin3D/CastingCanvas");
+        Node currentScene = GetTree().CurrentScene;
 
         if (IsMultiplayerAuthority())
         {
-            _error = currentScene.GetNode($"Viewport2Din3D/Viewport/SetupPanel").Connect("SpellDeSelected", new Callable(this, nameof(OnSpellDeSelected)));
+            _error = currentScene.GetNode("Viewport2Din3D/Viewport/SetupPanel").Connect("SpellDeSelected", new Callable(this, nameof(OnSpellDeSelected)));
             if (_error != Error.Ok) GD.PrintErr("Done messed up");
             else GD.Print("SpellDeselected connected successfully");
 
-            _error = currentScene.GetNode($"Viewport2Din3D/Viewport/SetupPanel").Connect("SpellSelected", new Callable(this, nameof(OnSpellSelected)));
+            _error = currentScene.GetNode("Viewport2Din3D/Viewport/SetupPanel").Connect("SpellSelected", new Callable(this, nameof(OnSpellSelected)));
             if (_error != Error.Ok) GD.PrintErr("Done messed up");
             else GD.Print("SpellSelected connected successfully");
 
             GetNode<Node3D>("XROrigin3D").SetProcess(true);
             GetNode<MeshInstance3D>("PlayerModel").Visible = false;
+
+            ConnectGrabSignals("XROrigin3D/LeftHand/Interactor");
+            ConnectGrabSignals("XROrigin3D/RightHand/Interactor");
         }
         else
         {
             GetNode<Node3D>("XROrigin3D").SetProcess(false);
             GetNode<MeshInstance3D>("PlayerModel").Visible = true;
+            GetNode<XRController3D>("XROrigin3D/LeftHand").SetProcess(false);
+            GetNode<XRController3D>("XROrigin3D/RightHand").SetProcess(false);
+        }
+    }
+
+    private void ConnectGrabSignals(string interactorPath)
+    {
+        var interactor = GetNodeOrNull(interactorPath);
+        if (interactor == null)
+        {
+            GD.PrintErr($"Could not find interactor at path: {interactorPath}");
+            return;
+        }
+
+        interactor.Connect("picked_up", new Callable(this, nameof(OnPickedUp)));
+        interactor.Connect("dropped", new Callable(this, nameof(OnDropped)));
+    }
+
+    private void OnPickedUp(Node3D pickedUpNode)
+    {
+        GD.Print($"Picked up: {pickedUpNode.Name}");
+
+        if (pickedUpNode is GrabbableObject grabbable)
+        {
+            grabbable.Grab(Multiplayer.GetUniqueId());
+            return;
+        }
+    }
+
+    private void OnDropped(Node3D droppedNode)
+    {
+        GD.Print($"Dropped: {droppedNode.Name}");
+
+        if (droppedNode.HasMethod("set_multiplayer_authority"))
+        {
+            droppedNode.Call("set_multiplayer_authority", 1);
         }
     }
 
@@ -64,5 +103,4 @@ public partial class Player : Node3D
             EmitSignal(SignalName.GlyphWakeUp, spell);
         }
     }
-
 }
